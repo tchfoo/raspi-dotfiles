ea68d61eb0471738613219356b0daf5509a5ea9c
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2023-10-28 12:05:47 +0200

    Add openssh

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,16 @@
 
   time.timeZone = "Europe/Budapest";
 
+  services.openssh = {
+    enable = true;
+    ports = [ 42727 ];
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+    };
+  };
+
   users.users.gep = {
     isNormalUser = true;
     extraGroups = [
