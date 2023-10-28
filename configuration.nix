315b77505dd195831e7fea6ec668e6d5e09d2209
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2023-10-28 12:05:57 +0200

    Allow gep to log in with openssh

--- configuration.nix
+++ configuration.nix
@@ -23,6 +23,14 @@
     extraGroups = [
       "wheel"
     ];
+    openssh.authorizedKeys.keys = [
+      # geptop
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDPWyW2tBcCORf4C/Z7iPaKGoiswyLdds3m8ZrNY8OXl gutyina.gergo.2@gmail.com"
+      # geppc
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKGW5zKjn01DVf6vTs/D2VV+/awXTNboY1iaCThi2A1v gep@geppc"
+      # gepphone
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQXysKutq2b67RAmq46qMH8TDLEYf0D5SYon4vE6efO u0_a483@localhost"
+    ];
   };
 
   system.stateVersion = "23.05";
