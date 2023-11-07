d860c7178f9e9c54f43e8d6b235529de8b0ba51f
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2023-11-07 15:23:17 +0100

    Remove unnecessary php-fpm options

--- configuration.nix
+++ configuration.nix
@@ -27,9 +27,6 @@ in
       "pm.start_servers" = 2;
       "pm.min_spare_servers" = 2;
       "pm.max_spare_servers" = 5;
-      "php_admin_value[error_log]" = "stderr";
-      "php_admin_flag[log_errors]" = true;
-      "catch_workers_output" = true;
       "security.limit_extensions" = ".php .html";
     };
     phpEnv."PATH" = lib.makeBinPath [ pkgs.php ];
