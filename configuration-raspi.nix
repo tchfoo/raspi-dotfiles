661560e01dd938e0f6e44921a8a326c4c1c7079d
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2023-12-01 22:48:10 +0100

    Update configuration-raspi.nix

--- configuration-raspi.nix
+++ configuration-raspi.nix
@@ -1,6 +1,11 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib ... }:
 
 {
+  # NixOS wants to enable GRUB by default
+  boot.loader.grub.enable = false;
+  # Enables the generation of /boot/extlinux/extlinux.conf
+  boot.loader.generic-extlinux-compatible.enable = true;
+
   imports = [
     ./configuration.nix
     ./hardware-configuration-raspi.nix
