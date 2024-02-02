e0d41437acc0164167d71533106dfcf75b4bf5b0
YMSTNT <21342713+YMSTNT@users.noreply.github.com>
2024-02-02 13:52:19 +0100

    Update ymstnt's rebuild command

--- configuration.nix
+++ configuration.nix
@@ -305,7 +305,7 @@ in
     programs.bash = {
       enable = true;
       shellAliases = {
-        rebuild = "(cd $HOME/raspi-dotfiles && bash ./rebuild.sh .#raspi)";
+        rebuild = "(cd $HOME/raspi-dotfiles && sudo nixos-rebuild switch --flake .#raspi --impure)";
         update = "(cd $HOME/raspi-dotfiles && nix flake update --commit-lock-file)";
         dotcd = "cd $HOME/raspi-dotfiles";
         bashreload = "source $HOME/.bashrc";
