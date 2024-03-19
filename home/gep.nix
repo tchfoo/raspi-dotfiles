a462517afdbf433780b5d1f1c7741572a7276dec
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-03-19 10:48:48 +0100

    Remove unused ssh key from gep

--- home/gep.nix
+++ home/gep.nix
@@ -15,8 +15,6 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKGW5zKjn01DVf6vTs/D2VV+/awXTNboY1iaCThi2A1v gep@geppc"
       # gepphone
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQXysKutq2b67RAmq46qMH8TDLEYf0D5SYon4vE6efO u0_a483@localhost"
-      # remote access to vm
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOXQoNrfIJBVG52vq8igC0LvG53dGYfGayWebIymgk1Y ymstnt@geppc"
     ];
   };
 }
