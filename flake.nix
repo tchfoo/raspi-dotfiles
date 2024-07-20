7a2f6966cea8e0c61e0b11df7308a3f47b4445e4
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-07-20 22:52:37 +0200

    flake: remove unused dependencies

--- flake.nix
+++ flake.nix
@@ -4,7 +4,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    agenix.url = "github:ryantm/agenix";
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+      inputs.systems.follows = "systems";
+      inputs.darwin.follows = "";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,18 +18,34 @@
     moe = {
       url = "github:YMSTNT/moe";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
     gep-dotfiles = {
       url = "github:gepbird/dotfiles/nixos";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.agenix.follows = "agenix";
       inputs.home-manager.follows = "home-manager";
+      inputs.systems.follows = "systems";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nix-matlab.follows = "";
+      inputs.dwm-gep.follows = "";
+      inputs.nixpkgs-nvidia.follows = "";
     };
     ymstnt-dotfiles = {
       url = "github:ymstnt/dotfiles/main";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.agenix.follows = "agenix";
       inputs.home-manager.follows = "home-manager";
+      inputs.nix-matlab.follows = "";
+      inputs.nixpkgs-master.follows = "";
+      inputs.nixpkgs-develop.follows = "";
+      inputs.nixos-cosmic.follows = "";
+    };
+    # dependencies of the above modules
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
     };
   };
 
