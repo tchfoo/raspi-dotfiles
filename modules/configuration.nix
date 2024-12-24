802b0a6b9d5e204edabf078028db4450bfb9aa4a
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-12-24 21:37:55 +0100

    modules: migrate transmission

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -23,17 +23,6 @@
 
   systemd.tmpfiles.rules = [
     # Type Path                           Mode User   Group   Age Argument
-    " d    /var/media                     0755 ymstnt shared"
-    " d    /var/media/music               0755 ymstnt shared"
-    " d    /var/media/torrents            0755 ymstnt shared"
-    " d    /var/media/incomplete-torrents 0755 ymstnt shared"
-    " d    /var/media/torrents/Movies     0755 ymstnt shared"
-    " d    /var/media/torrents/Shows      0755 ymstnt shared"
-    " d    /var/media/torrents/Anime      0755 ymstnt shared"
-    " d    /var/media/media-server        0755 ymstnt shared"
-    " d    /var/media/media-server/Movies 0755 ymstnt shared"
-    " d    /var/media/media-server/Shows  0755 ymstnt shared"
-    " d    /var/media/media-server/Anime  0755 ymstnt shared"
     " d    /var/moe                       0750 moe    shared"
     " d    /var/www/ymstnt.com            2770 nginx  shared"
     " d    /var/www/ymstnt.com-generated  0775 shared shared"
@@ -42,7 +31,6 @@
   age.secrets = {
     moe.file = ../secrets/moe.age;
     mysql.file = ../secrets/mysql.age;
-    transmission.file = ../secrets/transmission.json.age;
     runner1.file = ../secrets/runner1.age;
     miniflux.file = ../secrets/miniflux.age;
     gotosocial.file = ../secrets/gotosocial.age;
@@ -51,29 +39,6 @@
   };
 
   services.avahi.enable = true;
-
-  services.transmission = {
-    user = "ymstnt";
-    group = "shared";
-    enable = true;
-    openRPCPort = true;
-    openPeerPorts = true;
-    settings = {
-      download-dir = "/var/media/torrents";
-      incomplete-dir = "/var/media/incomplete-torrents";
-      rpc-enabled = true;
-      rpc-host-whitelist-enabled = false;
-      rpc-whitelist-enabled = true;
-      rpc-authentication-required = true;
-      rpc-username = "ymstnt";
-      rpc-whitelist = "127.0.0.1,192.168.*.*,100.*.*.*";
-      rpc-bind-address = "0.0.0.0";
-      umask = 18;
-      ratio-limit = 1;
-      ratio-limit-enabled = true;
-    };
-    credentialsFile = config.age.secrets.transmission.path;
-  };
 
   services.github-runners = {
     website = {
