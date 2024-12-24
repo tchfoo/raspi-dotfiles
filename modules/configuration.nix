40d689b8af2af5933a0663692ad25e997e1cc66b
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-12-24 22:12:58 +0100

    modules: migrate ntfy-sh

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -52,16 +52,6 @@
     };
   };
 
-  services.ntfy-sh = {
-    enable = true;
-    group = "shared";
-    settings = {
-      base-url = "https://ntfy.ymstnt.com";
-      behind-proxy = true;
-      web-root = "disable";
-    };
-  };
-
   services.phpfpm.pools.shared = {
     user = "shared";
     settings = {
@@ -134,17 +124,6 @@
           "/\.git".extraConfig = ''
             deny all;
           '';
-        };
-      };
-      "ntfy.ymstnt.com" = {
-        enableACME = true;
-        forceSSL = true;
-        locations = {
-          "/" = {
-            proxyPass = "http://${toString config.services.ntfy-sh.settings.listen-http}";
-            recommendedProxySettings = true;
-            proxyWebsockets = true;
-          };
         };
       };
     };
