33cf250252ec5bec807ef339c506f8a683fab48c
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-12-25 10:35:37 +0100

    modules: migrate nginx

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -15,21 +15,6 @@
 
   time.timeZone = "Europe/Budapest";
 
-  services.nginx = {
-    enable = true;
-    group = "shared";
-    appendConfig = ''
-      error_log /var/log/nginx/error.log debug;
-    '';
-  };
-
-  security.acme = {
-    acceptTerms = true;
-    defaults.email = "ymstnt@mailbox.org";
-  };
-
-  networking.firewall.allowedTCPPorts = [ 80 443 ];
-
   environment.shellInit = "umask 002";
   users.users = {
     shared = {
---
+++ modules/nginx.nix
@@ -0,0 +1,21 @@
+{ ... }:
+
+{
+  services.nginx = {
+    enable = true;
+    group = "shared";
+    appendConfig = ''
+      error_log /var/log/nginx/error.log debug;
+    '';
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "ymstnt@mailbox.org";
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+}
