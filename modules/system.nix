613ef5891a3c0e87b1b9ef5618cabd9b0479127f
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-12-25 11:40:15 +0100

    modules: migrate networking

--- modules/system.nix
+++ modules/system.nix
@@ -3,11 +3,6 @@
 {
   console.keyMap = "hu";
 
-  networking = {
-    hostName = "raspi-doboz";
-    networkmanager.enable = true;
-  };
-
   time.timeZone = "Europe/Budapest";
 
   environment.shellInit = "umask 002";
@@ -19,8 +14,4 @@
   };
 
   users.groups.shared = { };
-
-  # TODO: remove after issue is fixed https://github.com/NixOS/nixpkgs/issues/180175
-  #systemd.services.tailscaled.after = ["NetworkManager-wait-online.service"];
-  systemd.services.NetworkManager-wait-online.enable = false;
 }
