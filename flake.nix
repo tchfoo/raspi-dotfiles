ef3a7bdaf61ad1de78009024138c7909fa989e68
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-01-03 20:16:30 +0100

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'gep-dotfiles':
        'github:gepbird/dotfiles/b688a5765b0f73bde242f76d3e4894178f31fd11' (2024-12-22)
      → 'github:gepbird/dotfiles/c77c8697e6be79c2e0274258950d9416ef65d394' (2025-01-03)
    • Updated input 'gep-dotfiles/flake-parts':
        'github:hercules-ci/flake-parts/205b12d8b7cd4802fbcb8e8ef6a0f1408781a4f9' (2024-12-04)
      → 'github:hercules-ci/flake-parts/f2f7418ce0ab4a5309a4596161d154cfc877af66' (2025-01-01)
    • Updated input 'gep-dotfiles/git-hooks':
        'github:cachix/git-hooks.nix/0ddd26d0925f618c3a5d85a4fa5eb1e23a09491d' (2024-12-17)
      → 'github:cachix/git-hooks.nix/a5a961387e75ae44cc20f0a57ae463da5e959656' (2025-01-03)
    • Removed input 'gep-dotfiles/git-hooks/nixpkgs-stable'
    • Updated input 'gep-dotfiles/hercules-ci-effects':
        'github:hercules-ci/hercules-ci-effects/56f8ea8d502c87cf62444bec4ee04512e8ea24ea' (2024-12-04)
      → 'github:hercules-ci/hercules-ci-effects/f6233b5cfbada692d93a73d6ed35bdbfd0fdb9c4' (2025-01-01)
    • Updated input 'gep-dotfiles/neovim-nightly':
        'github:nix-community/neovim-nightly-overlay/d3963249e534e247041862b8162fb738c8604d3a' (2024-12-20)
      → 'github:nix-community/neovim-nightly-overlay/54dd5a7c0384fdcd11830e52f9896e457f189cb6' (2025-01-03)
    • Updated input 'gep-dotfiles/neovim-nightly/neovim-src':
        'github:neovim/neovim/8ef41f590224dfeea2e51d9fec150e363fd72ee0' (2024-12-19)
      → 'github:neovim/neovim/c26951b1d6d4d7ff8fe431e8bfb16744ff56af1c' (2025-01-03)
    • Removed input 'gep-dotfiles/nixpkgs-anydesk'
    • Removed input 'gep-dotfiles/nixpkgs-nvidia'
    • Updated input 'home-manager':
        'github:nix-community/home-manager/35b98d20ca8f4ca1f6a2c30b8a2c8bb305a36d84' (2024-12-24)
      → 'github:nix-community/home-manager/ef64efdbaca99f9960f75efab991e4c49e79a5f1' (2025-01-03)
    • Updated input 'nixos-hardware':
        'github:NixOS/nixos-hardware/def1d472c832d77885f174089b0d34854b007198' (2024-12-23)
      → 'github:NixOS/nixos-hardware/7c674c6734f61157e321db595dbfcd8523e04e19' (2024-12-28)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/d70bd19e0a38ad4790d3913bf08fcbfc9eeca507' (2024-12-19)
      → 'github:NixOS/nixpkgs/6df24922a1400241dae323af55f30e4318a6ca65' (2025-01-02)
    • Updated input 'ymstnt-dotfiles':
        'github:ymstnt/dotfiles/02548d7f546aef7cc19d26449402962deead47a6' (2024-12-25)
      → 'github:ymstnt/dotfiles/454e76bd3146eb30a516377008c8bb990fea5f0c' (2024-12-31)

--- flake.nix
+++ flake.nix
@@ -29,8 +29,6 @@
     gep-dotfiles = {
       url = "github:gepbird/dotfiles/nixos";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nixpkgs-nvidia.follows = "";
-      inputs.nixpkgs-anydesk.follows = "";
       inputs.agenix.follows = "agenix";
       inputs.home-manager.follows = "home-manager";
       inputs.systems.follows = "systems";
