e2897b098b589d6d33020309e511e792d13a0122
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-04-14 22:35:30 +0200

    ymstnt.com: replace with new site

--- modules/ymstnt.com.nix
+++ modules/ymstnt.com.nix
@@ -6,46 +6,6 @@
 }:
 
 {
-  services.github-runners = {
-    website = rec {
-      enable = true;
-      replace = true;
-      user = "shared";
-      url = "https://github.com/ymstnt/ymstnt.com";
-      tokenFile = config.age.secrets.runner1.path;
-      extraPackages = with pkgs; [
-        nodejs_20
-      ];
-      extraEnvironment = {
-        OUT_DIR = "/var/www/ymstnt.com-generated";
-      };
-      nodeRuntimes = [ "node20" ];
-      serviceOverrides = {
-        ReadWritePaths = [ extraEnvironment.OUT_DIR ];
-      };
-    };
-  };
-
-  services.nginx.virtualHosts."ymstnt.com" = {
-    enableACME = true;
-    forceSSL = true;
-    root = "/var/www/ymstnt.com-generated";
-    extraConfig = ''
-      error_page 404 /404.html;
-    '';
-    locations = {
-      "~ ^/(auth|explode|gepDrive|geputils|header|libs|global.css)".extraConfig = ''
-        return 301 https://gd.tchfoo.com$request_uri;
-      '';
-      "~ ^/seboard(/.*)?$".extraConfig = ''
-        if ($1 = "") {
-          set $1 "/";
-        }
-        return 301 https://sb.tchfoo.com$1;
-      '';
-    };
-  };
-
   services.phpfpm.pools."ymstnt.com" = {
     user = "shared";
     settings = {
@@ -63,7 +23,7 @@
       ];
   };
 
-  services.nginx.virtualHosts."test.tchfoo.com" = {
+  services.nginx.virtualHosts."ymstnt.com" = {
     enableACME = true;
     forceSSL = true;
     root = "/var/www/ymstnt.com";
@@ -91,6 +51,6 @@
 
   systemd.tmpfiles.rules = [
     # Type Path                           Mode User   Group   Age Argument
-    " d    /var/www/ymstnt.com-generated  0775 shared shared"
+    " d    /var/www/ymstnt.com            0775 shared shared"
   ];
 }
