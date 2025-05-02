adeaed11bf43958eba79f7ed6d4b82f40a6c6839
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-05-02 23:50:03 +0200

    feat(gep): use ssh from dotfiles

--- modules/gep.nix
+++ modules/gep.nix
@@ -23,6 +23,7 @@
     php
     python
     rust
+    ssh
     starship
     tmux
     webdev
@@ -32,26 +33,12 @@
 
   hm-gep.programs.nh.flake = lib.mkForce "${config.hm-gep.home.homeDirectory}/raspi-dotfiles";
 
-  hm-gep.services.ssh-agent = {
-    enable = true;
-  };
-
   users.users.gep = {
     initialPassword = "gep";
     isNormalUser = true;
     extraGroups = [
       "wheel"
       "shared"
-    ];
-    openssh.authorizedKeys.keys = [
-      # geptop
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDPWyW2tBcCORf4C/Z7iPaKGoiswyLdds3m8ZrNY8OXl gutyina.gergo.2@gmail.com"
-      # geppc
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKGW5zKjn01DVf6vTs/D2VV+/awXTNboY1iaCThi2A1v gep@geppc"
-      # gepphone
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQXysKutq2b67RAmq46qMH8TDLEYf0D5SYon4vE6efO u0_a483@localhost"
-      # gepwin
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM5plUDJC9i4F1FG7+lZvWFyYDhse98E94usal1yr3gE gep@DESKTOP-3LEAJ4K"
     ];
   };
 }
