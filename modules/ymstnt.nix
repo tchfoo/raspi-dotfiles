1d37a5e0e51dc649d87603d112d0d733f0c6ba2c
ymstnt <21342713+ymstnt@users.noreply.github.com>
2025-05-03 16:04:21 +0200

    feat(ymstnt): use ssh keys from dotfiles

--- modules/ymstnt.nix
+++ modules/ymstnt.nix
@@ -27,16 +27,6 @@
       "wheel"
       "shared"
     ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJLWg7uXAd3GfBmXV5b9iLp+EZ9rfu+gRWWCb8YXML4o u0_a557@localhost"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDVor+g/31/XFIzuZYQrNK/RIbU1iDaSyOfM8re73eAd ymstnt@cassiopeia"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGx6TyqDxyb74F0rjyCu/9z4QO2pX6tmJdb3m62QrQrg ymstnt@cassiopeia-win"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEVxinYyV/gDhWNeSa0LD6kRKwTWhFxXVS23axGO/2sa ymstnt@andromeda"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKV37wsI1w67r267Tq1J4qGlym2eTdcOBs6jtlUpu3UJ ymstnt@andromeda-win"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKLQKmZDSyZvpXqaqLigdrQEJzrcu4ry0zGydZipliPZ u0_a293@localhost"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICRh9g9ZttYswCxdIE7KYL3xs4JZqhDCUc5BYjDMxFph u0_a355@localhost"
-      "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAAuZLBwbD5ZkDy70jKmOPrmtPtqLAddpjFDWS8mTfF/CdFAIwgoU/tMTxIKSx0PRjUqXzcO9JhvI+9/QeswBw27PAFBdmBmMKiJqbrZEPZv3ecL/Qd6KGAsB2GGGWTF82+gL5Vb1Sm3cjJnQUBGJoIz6tam5Ya5VRUUXxe1X52uS70mhA== u0_a355@localhost"
-    ];
   };
 
   home-manager.users.ymstnt = {
