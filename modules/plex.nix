eba2fcfb9ce33e4f9a9ff7d4eb88a6097c243b8d
ymstnt <21342713+YMSTNT@users.noreply.github.com>
2025-05-15 19:25:49 +0200

    feat(plex): add /hdd media folders declaratively

--- modules/plex.nix
+++ modules/plex.nix
@@ -9,4 +9,12 @@
     user = "ymstnt";
     group = "shared";
   };
+
+  systemd.tmpfiles.rules = [
+    # Type Path                           Mode User   Group   Age Argument
+    " d    /hdd/media                     0755 ymstnt shared"
+    " d    /hdd/media/music               0755 ymstnt shared"
+    " d    /hdd/media/movies              0755 ymstnt shared"
+    " d    /hdd/media/shows               0755 ymstnt shared"
+  ];
 }
