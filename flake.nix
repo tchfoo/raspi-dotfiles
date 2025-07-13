450b4d6060b607bbe506b3cd4ffce19ea8767959
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-07-13 22:37:54 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'gep-dotfiles':
        'github:gepbird/dotfiles/ae541f7065d3ea367150add90d6d6926a30c1a48' (2025-07-05)
      → 'github:gepbird/dotfiles/2c44237b4da19ea96422e73b044d49d2c26207bf' (2025-07-12)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/5a49fe448e81ee05ab48ed3189109b322237ddbf' (2025-07-05)
      → 'github:nix-community/home-manager/bf893ad4cbf46610dd1b620c974f824e266cd1df' (2025-07-13)
    • Updated input 'moe':
        'github:ymstnt-com/moe/28ab5a3094fbc009a84fd0efae5282621984cd93' (2025-05-27)
      → 'github:ymstnt-com/moe/1b554861d81363e4fcf3e1da13817ba8ae1feb70' (2025-07-13)
    • Updated input 'moe/nuget-packageslock2nix':
        'github:mdarocha/nuget-packageslock2nix/d483a466e5219674bbecd58320b68f91177f0acf' (2025-05-27)
      → 'github:mdarocha/nuget-packageslock2nix/9a101f9ca1feb1fad676a2c353dc76b8fc9ec66e' (2025-07-08)
    • Updated input 'nixos-hardware':
        'github:NixOS/nixos-hardware/497ae1357f1ac97f1aea31a4cb74ad0d534ef41f' (2025-07-02)
      → 'github:NixOS/nixos-hardware/7ced9122cff2163c6a0212b8d1ec8c33a1660806' (2025-07-09)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/5c724ed1388e53cc231ed98330a60eb2f7be4be3' (2025-07-04)
      → 'github:NixOS/nixpkgs/9807714d6944a957c2e036f84b0ff8caf9930bc0' (2025-07-08)
    • Removed input 'nixpkgs-patch-pocket-id-dev-1-5-0'
    • Updated input 'ymstnt-dotfiles':
        'github:ymstnt/dotfiles/bd27d38f2343b99c8f6d2c7dec02c3f47f3e1683' (2025-07-04)
      → 'github:ymstnt/dotfiles/25e65917af708d2ebaaea4a1a4e9275baebd9a54' (2025-07-13)
    • Updated input 'ymstnt-dotfiles/nixpkgs-patch-vencord':
        'https://github.com/nixos/nixpkgs/pull/422168.diff?narHash=sha256-zW4zkNBRxOYqBj2dwjVjOGKKni6EOD8VSC3AcRu3E6o%3D'
      → 'https://github.com/nixos/nixpkgs/pull/422168.diff?narHash=sha256-rER/6T/R61JvXV7McdovAJ5iPstw2YJhHUy2cGMJHms%3D'

--- flake.nix
+++ flake.nix
@@ -30,10 +30,6 @@
       inputs.flake-utils.follows = "flake-utils";
     };
     nixpkgs-patcher.url = "github:gepbird/nixpkgs-patcher";
-    nixpkgs-patch-pocket-id-dev-1-5-0 = {
-      url = "https://github.com/NixOS/nixpkgs/pull/422570.diff";
-      flake = false;
-    };
     nixpkgs-patch-pocket-id-dev-1-6-1 = {
       url = "https://github.com/NixOS/nixpkgs/pull/423723.diff";
       flake = false;
