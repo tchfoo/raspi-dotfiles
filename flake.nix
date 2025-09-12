02338fa86dea6a607edfca4afaadd77585729d50
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-09-12 19:14:11 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'gep-dotfiles':
        'github:gepbird/dotfiles/42ba5c4545d2fe429225215dc95b2bcf369d7bf3' (2025-08-31)
      → 'github:gepbird/dotfiles/c2cbf33231e0ea02109effa7945f824f86291f62' (2025-09-11)
    • Updated input 'gep-dotfiles/flake-parts':
        'github:hercules-ci/flake-parts/af66ad14b28a127c5c0f3bbb298218fc63528a18' (2025-08-06)
      → 'github:hercules-ci/flake-parts/4524271976b625a4a605beefd893f270620fd751' (2025-09-01)
    • Updated input 'gep-dotfiles/nix-index-database':
        'github:nix-community/nix-index-database/3fe768e1f058961095b4a0d7a2ba15dc9736bdc6' (2025-08-31)
      → 'github:nix-community/nix-index-database/9b144dc3ef6e42b888c4190e02746aab13b0e97f' (2025-09-07)
    • Added input 'gep-dotfiles/nixpkgs-patch-rnote-0-13-0':
        'https://github.com/NixOS/nixpkgs/pull/440811.diff?narHash=sha256-ovWGUyWjUphkOm87v5gfK9wezMuoqJf6qj8Te1cL86w%3D'
    • Updated input 'gep-dotfiles/nvim':
        'github:gepbird/nvim/07f41f48e01600e5ed47a910a33534d5c709ab6a' (2025-08-30)
      → 'github:gepbird/nvim/e5c34da926a8aed8fabbe1d5c23e8b2de4608f94' (2025-09-06)
    • Updated input 'gep-dotfiles/nvim/neovim-nightly':
        'github:nix-community/neovim-nightly-overlay/3edf4118b2bb7182ceabbd9ce0a374c415e5a793' (2025-08-29)
      → 'github:nix-community/neovim-nightly-overlay/b447e494cafb7adaf396714a285245e4d1536a6a' (2025-09-06)
    • Updated input 'gep-dotfiles/nvim/neovim-nightly/neovim-src':
        'github:neovim/neovim/19f2e5c3eb4194a4b4255abb01991af63c1979b3' (2025-08-28)
      → 'github:neovim/neovim/069be911064d0e64f1efc13348dbb878afaf347f' (2025-09-05)
    • Updated input 'gep-dotfiles/nvim/nixvim':
        'github:nix-community/nixvim/b7e96214e8e7244eceae73c606dcd243f6d180a3' (2025-08-27)
      → 'github:nix-community/nixvim/7afdd40b96c9168aa4cb49b86fc67eccd441cae5' (2025-09-06)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/f56bf065f9abedc7bc15e1f2454aa5c8edabaacf' (2025-09-05)
      → 'github:nix-community/home-manager/9eab59f3e71ea3a725e4817d8dcf0da0824ad19d' (2025-09-12)
    • Updated input 'moe':
        'github:ymstnt-com/moe/61d328888192e2fd6da574ba5fb7792694ccc883' (2025-08-05)
      → 'github:ymstnt-com/moe/fadbbe84dd275581b6941d403596b8e70e78d6ed' (2025-09-12)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/d0fc30899600b9b3466ddb260fd83deb486c32f1' (2025-09-02)
      → 'github:NixOS/nixpkgs/ab0f3607a6c7486ea22229b92ed2d355f1482ee0' (2025-09-10)
    • Removed input 'nixpkgs-patch-pocket-id-1-10-0'
    • Updated input 'nixpkgs-patch-rauthy-init':
        'https://github.com/NixOS/nixpkgs/pull/371091.diff?narHash=sha256-%2BhdQIZf7fuJk6V4higAAtT/1jtxVj9g%2BOWCx6As/Uqo%3D'
      → 'https://github.com/NixOS/nixpkgs/pull/371091.diff?narHash=sha256-R5W0WPwvH6IaxrcskH0iwTiZadQrQzsvbcHnCnwzKz4%3D'
    • Updated input 'nur':
        'github:nix-community/NUR/03e674eeebcdd82b10241b7d331ff03b0794c858' (2025-09-11)
      → 'github:nix-community/NUR/4a083b46f2c333354531fad382d909d0f218fe3a' (2025-09-12)
    • Updated input 'ymstnt-dotfiles':
        'github:ymstnt/dotfiles/36dc3a4aa51c53809527f4e5376d43fdd51380ce' (2025-09-03)
      → 'github:ymstnt/dotfiles/5318b548df0e809ea935427bac7ab7861a6cc918' (2025-09-08)

--- flake.nix
+++ flake.nix
@@ -34,10 +34,6 @@
       url = "https://github.com/NixOS/nixpkgs/pull/371091.diff";
       flake = false;
     };
-    nixpkgs-patch-pocket-id-1-10-0 = {
-      url = "https://github.com/NixOS/nixpkgs/pull/440172.diff";
-      flake = false;
-    };
     gep-dotfiles = {
       url = "github:gepbird/dotfiles";
       inputs.nixpkgs.follows = "nixpkgs";
