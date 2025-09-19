808e1ab3a67633c0449da0be9f952b2da58ebb4e
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-09-19 14:58:47 +0200

    earlyoom: init

---
+++ modules/earlyoom.nix
@@ -0,0 +1,20 @@
+{
+  ...
+}:
+
+{
+  services.earlyoom = {
+    enable = true;
+    reportInterval = 60;
+    freeSwapThreshold = 20;
+    freeMemThreshold = 20;
+    freeSwapKillThreshold = 10;
+    freeMemKillThreshold = 15;
+    extraArgs = [
+      "--prefer"
+      "(systemd-coredum|nixd|clangd|OmniSharp|rust-analyzer|nvim)"
+      "--ignore"
+      "(sshd|sshd-session)"
+    ];
+  };
+}
