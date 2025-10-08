569351607abe25c328a349ee86d0f08978c42b6e
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-10-08 06:27:21 +0200

    feat(pocket-id): 1.11.2 -> 1.13.1

--- flake.nix
+++ flake.nix
@@ -35,6 +35,10 @@
       url = "https://github.com/NixOS/nixpkgs/pull/371091.diff";
       flake = false;
     };
+    nixpkgs-patch-pocket-id-1-13-1 = {
+      url = "https://github.com/NixOS/nixpkgs/pull/449674.diff";
+      flake = false;
+    };
     gep-dotfiles = {
       url = "github:gepbird/dotfiles";
       inputs.nixpkgs.follows = "nixpkgs";
