ba4d2dfc7139a8325f5d62df8c42cd02ce1a1fc3
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-11-14 14:23:45 +0100

    feat(flake): add substituters in nixConfig

--- flake.nix
+++ flake.nix
@@ -118,4 +118,15 @@
           ;
       };
     };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://nixos-raspberrypi.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+    ];
+  };
 }
