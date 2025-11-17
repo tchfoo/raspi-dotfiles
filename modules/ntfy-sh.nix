eacc080a5dbb1bd1d5364a2034aa2c0ddad8be7a
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-11-17 11:38:03 +0100

    feat(ntfy-sh): ntfy.ymstnt.com -> ntfy.tchfoo.com

--- modules/ntfy-sh.nix
+++ modules/ntfy-sh.nix
@@ -8,21 +8,20 @@
     enable = true;
     group = "shared";
     settings = {
-      base-url = "https://ntfy.ymstnt.com";
+      base-url = "https://ntfy.tchfoo.com";
       behind-proxy = true;
       web-root = "disable";
     };
   };
 
-  services.nginx.virtualHosts."ntfy.ymstnt.com" = {
+  services.nginx.virtualHosts."ntfy.tchfoo.com" = {
     enableACME = true;
     forceSSL = true;
-    locations = {
-      "/" = {
-        proxyPass = "http://${toString config.services.ntfy-sh.settings.listen-http}";
-        recommendedProxySettings = true;
-        proxyWebsockets = true;
-      };
+    locations."/" = {
+      proxyPass = "http://${toString config.services.ntfy-sh.settings.listen-http}";
+      recommendedProxySettings = true;
+      proxyWebsockets = true;
     };
   };
 }
+
