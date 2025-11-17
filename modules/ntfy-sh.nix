ae24761fe4db44eb927f3d3b2baaece14497b32c
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-11-17 20:24:49 +0100

    feat(ntfy-sh): move from ntfy.ymstnt.com to services.tchfoo.com/ntfy

--- modules/ntfy-sh.nix
+++ modules/ntfy-sh.nix
@@ -8,21 +8,15 @@
     enable = true;
     group = "shared";
     settings = {
-      base-url = "https://ntfy.ymstnt.com";
+      base-url = "https://services.tchfoo.com/ntfy";
       behind-proxy = true;
       web-root = "disable";
     };
   };
 
-  services.nginx.virtualHosts."ntfy.ymstnt.com" = {
-    enableACME = true;
-    forceSSL = true;
-    locations = {
-      "/" = {
-        proxyPass = "http://${toString config.services.ntfy-sh.settings.listen-http}";
-        recommendedProxySettings = true;
-        proxyWebsockets = true;
-      };
-    };
+  services.nginx.virtualHosts."services.tchfoo.com".locations."/ntfy" = {
+    proxyPass = "http://${toString config.services.ntfy-sh.settings.listen-http}";
+    recommendedProxySettings = true;
+    proxyWebsockets = true;
   };
 }
