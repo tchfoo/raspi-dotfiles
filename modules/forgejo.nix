2ec90fe87f6a7ad04fe631daaf0edf57310bab34
ymstnt <21342713+YMSTNT@users.noreply.github.com>
2025-12-26 10:32:29 +0100

    feat(forgejo): enable mailer

--- modules/forgejo.nix
+++ modules/forgejo.nix
@@ -5,11 +5,28 @@ let
 in
 {
   services.forgejo = {
+    secrets = {
+      mailer.FROM = config.secrets.forgejo.mailer.FROM;
+      mailer.SMTP_ADDR = config.secrets.forgejo.mailer.SMTP_ADDR;
+      mailer.USER = config.secrets.forgejo.mailer.USER;
+      mailer.PASSWD = config.secrets.forgejo.mailer.PASSWD;
+    };
     enable = true;
     settings = {
       server = {
         PROTOCOL = "http+unix";
         ROOT_URL = "https://git.tchfoo.com";
+      };
+      mailer = {
+        ENABLED = true;
+        PROTOCOL = "smtps";
+        SMTP_PORT = 465;        
+      };
+      service = {
+        ENABLE_NOTIFY_MAIL = true;
+      };
+      admin = {
+        SEND_NOTIFICATION_EMAIL_ON_NEW_USER = true;
       };
     };
   };
