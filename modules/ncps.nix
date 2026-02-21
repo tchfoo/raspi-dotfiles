5fea9925105186cef3e84f2944064235ced45130
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-02-21 17:00:54 +0100

    feat(ncps): add gepbird and ymstnt nur caches

--- modules/ncps.nix
+++ modules/ncps.nix
@@ -20,11 +20,15 @@ in
           "https://cache.gepbird.ovh"
           "https://cache.nixos.org"
           "https://nix-community.cachix.org"
+          "https://gepbird-nur-packages.cachix.org"
+          "https://ymstnt-nur-packages.cachix.org"
         ];
         publicKeys = [
           "nix-cache.tchfoo.com-1:pWK4l0phRA3bE0CviZodEQ5mWAQYoiuVi2LML+VNtNY="
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "gepbird-nur-packages.cachix.org-1:Ip2iveknanFBbJ2DFWk8cDomfRquUJiMWS/2fSeuMis="
+          "ymstnt-nur-packages.cachix.org-1:6XI6/GtEZmGUEYQsK5gUBrEMGTSnAN6xq8Vg++DA/lc="
         ];
       };
     };
