3695e0cd85b329014a58cb94ae7ca2163bc50543
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-02-22 00:46:46 +0100

    feat(ncps): update cache.gepbird.ovh key, note public key

--- modules/ncps.nix
+++ modules/ncps.nix
@@ -1,3 +1,4 @@
+# public key: "nix-cache.tchfoo.com-1:pWK4l0phRA3bE0CviZodEQ5mWAQYoiuVi2LML+VNtNY="
 {
   config,
   ...
@@ -24,7 +25,7 @@ in
           "https://ymstnt-nur-packages.cachix.org"
         ];
         publicKeys = [
-          "nix-cache.tchfoo.com-1:pWK4l0phRA3bE0CviZodEQ5mWAQYoiuVi2LML+VNtNY="
+          "cache.gepbird.ovh-1:3+1oLReKrK2xdXCcIgei+fdmP/F0+UYZA1uOMbVzWzE="
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "gepbird-nur-packages.cachix.org-1:Ip2iveknanFBbJ2DFWk8cDomfRquUJiMWS/2fSeuMis="
