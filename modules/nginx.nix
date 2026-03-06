d5f90bbe29cb1ee62988d7ef5a21488959b6de22
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-03-06 10:49:48 +0100

    Revert "fix(nginx): increase worker connections to avoid failed ncps requests"
    This reverts commit 6277d1c9d21bcb4d0da90a6b847841de833ad78f.

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -6,14 +6,7 @@
   services.nginx = {
     enable = true;
     group = "shared";
-    eventsConfig = ''
-      # workaround for https://github.com/kalbasit/ncps/issues/990
-      worker_connections 8192;
-    '';
     appendConfig = ''
-      # workaround for https://github.com/kalbasit/ncps/issues/990
-      worker_rlimit_nofile 65535;
-
       error_log /var/log/nginx/error.log debug;
     '';
   };
