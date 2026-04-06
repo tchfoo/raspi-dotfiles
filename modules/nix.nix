e81ad89ce584053f9b8f22f26a6b57bd3637e9e8
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-04-06 10:34:19 +0200

    feat(nix): drop flake-self-attrs

--- modules/nix.nix
+++ modules/nix.nix
@@ -14,11 +14,6 @@
         doCheck = false;
         doInstallCheck = false;
         patches = (o.patches or [ ]) ++ [
-          (prev.fetchpatch2 {
-            name = "add-inputs-self-submodules-flake-attribute.patch";
-            url = "https://git.lix.systems/lix-project/lix/commit/15a42d21a125ac58dc49b56c39497731344613e9.patch";
-            hash = "sha256-kNNBM1MnppWfP6TBzNzv4/sRinl9uK/dVU/aFxQV0iQ=";
-          })
           (prev.fetchurl {
             name = "trace-cache.patch";
             url = "https://git.lix.systems/gepbird/lix/compare/2.93.3...2.93.3-trace-cache-4.0.0.patch";
@@ -48,7 +43,6 @@
       experimental-features = [
         "nix-command"
         "flakes"
-        "flake-self-attrs"
       ];
       trusted-users = [
         "gep"
